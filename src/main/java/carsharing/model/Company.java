4f78963e12a977e858dae454e23332250fc870d9
Andrii Herman <andrii.herman.w@gmail.com>
2022-01-10 17:59:48 +0200

    Create Company model

---
+++ src/main/java/carsharing/model/Company.java
@@ -0,0 +1,11 @@
+package carsharing.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Company {
+    private int id;
+    private String name;
+}
