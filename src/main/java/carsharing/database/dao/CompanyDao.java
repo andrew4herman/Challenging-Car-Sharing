9aec394fd57e36b07cd1f8f095250711803b9c0e
Andrii Herman <andrii.herman.w@gmail.com>
2022-01-10 19:14:54 +0200

    Add getAll method to CompanyDao

--- src/main/java/carsharing/database/dao/CompanyDao.java
+++ src/main/java/carsharing/database/dao/CompanyDao.java
@@ -6,11 +6,15 @@ import carsharing.model.Company;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class CompanyDao {
 
     public final String GET_BY_ID = "SELECT * FROM company WHERE id = ?;";
+    public final String GET_All = "SELECT * FROM company;";
 
     private final DBManager manager;
 
@@ -32,5 +36,22 @@ public class CompanyDao {
             throw new RuntimeException("Cannot get from database a company with id " + id, e);
         }
         return Optional.empty();
+    }
+
+    public List<Company> getAll() {
+        List<Company> companies = new ArrayList<>();
+        try (PreparedStatement stmt =
+                     manager.getConnection().prepareStatement(GET_All)) {
+            ResultSet resultSet = stmt.executeQuery();
+
+            while (resultSet.next()) {
+                companies.add(new Company(
+                        resultSet.getInt("id"), resultSet.getString("name")));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Cannot get companies from database", e);
+        }
+
+        return companies;
     }
 }
