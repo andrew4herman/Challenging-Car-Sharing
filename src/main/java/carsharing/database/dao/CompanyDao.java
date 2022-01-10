aec9bd837c9ea24b2fa5c5cc43e36f9adde0eecd
Andrii Herman <andrii.herman.w@gmail.com>
2022-01-10 20:50:58 +0200

    Add save functionality in CompanyDao

--- src/main/java/carsharing/database/dao/CompanyDao.java
+++ src/main/java/carsharing/database/dao/CompanyDao.java
@@ -15,11 +15,15 @@ public class CompanyDao {
 
     public final String GET_BY_ID = "SELECT * FROM company WHERE id = ?;";
     public final String GET_All = "SELECT * FROM company;";
+    public final String SAVE_COMPANY = "INSERT INTO company(name) VALUES(?);";
+    public final String GET_LAST_ID = "SELECT MAX(id) as last_id FROM company;";
 
     private final DBManager manager;
+    private int lastCompanyId;
 
     public CompanyDao(DBManager manager) {
         this.manager = manager;
+        this.lastCompanyId = getLastCompanyId();
     }
 
     public Optional<Company> getById(int id) {
@@ -53,5 +57,35 @@ public class CompanyDao {
         }
 
         return companies;
+    }
+
+    public int save(String name) {
+        try (PreparedStatement stmt =
+                     manager.getConnection().prepareStatement(SAVE_COMPANY)) {
+            stmt.setString(1, name);
+
+            stmt.executeUpdate();
+            manager.getConnection().commit();
+
+            return ++lastCompanyId;
+        } catch (SQLException e) {
+            throw new RuntimeException("Cannot save company " + name, e);
+        }
+    }
+
+    private int getLastCompanyId() {
+        int lastId = 0;
+        try (PreparedStatement stmt =
+                     manager.getConnection().prepareStatement(GET_LAST_ID)) {
+            ResultSet resultSet = stmt.executeQuery();
+
+            if (resultSet.next()) {
+                lastId = resultSet.getInt("last_id");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Cannot get id of the last company", e);
+        }
+
+        return lastId;
     }
 }
