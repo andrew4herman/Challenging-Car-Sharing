6054eb67f653b7abb57d0e50b94fbcc6b122ff18
Andrii Herman <andrii.herman.w@gmail.com>
2022-01-10 19:06:51 +0200

    Add getById method to CompanyDao

--- src/main/java/carsharing/database/dao/CompanyDao.java
+++ src/main/java/carsharing/database/dao/CompanyDao.java
@@ -1,4 +1,36 @@
 package carsharing.database.dao;
 
+import carsharing.database.DBManager;
+import carsharing.model.Company;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+
 public class CompanyDao {
+
+    public final String GET_BY_ID = "SELECT * FROM company WHERE id = ?;";
+
+    private final DBManager manager;
+
+    public CompanyDao(DBManager manager) {
+        this.manager = manager;
+    }
+
+    public Optional<Company> getById(int id) {
+        try(PreparedStatement stmt =
+                manager.getConnection().prepareStatement(GET_BY_ID)) {
+            stmt.setInt(1, id);
+            ResultSet resultSet = stmt.executeQuery();
+
+            if (resultSet.next()) {
+                return Optional.of(new Company(
+                        resultSet.getInt("id"), resultSet.getString("name")));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Cannot get from database a company with id " + id, e);
+        }
+        return Optional.empty();
+    }
 }
