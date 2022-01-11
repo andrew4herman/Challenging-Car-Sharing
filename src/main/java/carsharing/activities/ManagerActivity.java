d983c6e5ab6069d8d498a6544734e56b197bc667
Andrii Herman <andrii.herman.w@gmail.com>
2022-01-11 19:45:09 +0200

    Replace functionality from CustomerActivity to ManagerActivity

--- src/main/java/carsharing/activities/ManagerActivity.java
+++ src/main/java/carsharing/activities/ManagerActivity.java
@@ -18,11 +18,32 @@ public class ManagerActivity extends Activity{
 
     @Override
     protected void showMenu() {
-
+        System.out.println("""
+                1. Company list
+                2. Create a company
+                0. Back
+                """);
     }
 
     @Override
     protected void processOption(String option) {
+        switch (option) {
+            case "0" -> System.out.println("Logged out!");
+            case "1" -> chooseCompanyOption();
+            case "2" -> createCompanyOption();
+            default -> System.out.println("Incorrect option. Try again!");
+        }
+    }
 
+    private void chooseCompanyOption() {
+
+    }
+
+    private void createCompanyOption() {
+        System.out.println("\nEnter the company name:");
+        String companyName = scanner.nextLine();
+
+        companyDao.save(companyName);
+        System.out.println("The company was created!");
     }
 }
