307f3050a94fa4e1ff045c29a404a63e0fc603b1
Andrii Herman <andrii.herman.w@gmail.com>
2022-01-11 14:42:54 +0200

    Initialize properties for ManagerActivity

--- src/main/java/carsharing/activities/ManagerActivity.java
+++ src/main/java/carsharing/activities/ManagerActivity.java
@@ -1,9 +1,27 @@
 package carsharing.activities;
 
+import carsharing.database.dao.CarDao;
+import carsharing.database.dao.CompanyDao;
+import carsharing.database.dao.CustomerDao;
+
+import java.util.Scanner;
+
 public class ManagerActivity implements Activity{
+
+    private final CompanyDao companyDao;
+    private final CarDao carDao;
+    private final CustomerDao customerDao;
+    private final Scanner scanner;
+
+    public ManagerActivity(CompanyDao companyDao, CarDao carDao, CustomerDao customerDao, Scanner scanner) {
+        this.companyDao = companyDao;
+        this.carDao = carDao;
+        this.customerDao = customerDao;
+        this.scanner = scanner;
+    }
 
     @Override
     public void start() {
-
+        
     }
 }
