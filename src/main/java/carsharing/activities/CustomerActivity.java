6fee81c54ff73f0d84567e8f13756d370407eb3f
Andrii Herman <andrii.herman.w@gmail.com>
2022-01-11 14:40:18 +0200

    Initialize properties for CustomerActivity

--- src/main/java/carsharing/activities/CustomerActivity.java
+++ src/main/java/carsharing/activities/CustomerActivity.java
@@ -1,6 +1,21 @@
 package carsharing.activities;
 
+import carsharing.database.dao.CarDao;
+import carsharing.database.dao.CompanyDao;
+
+import java.util.Scanner;
+
 public class CustomerActivity implements Activity {
+
+    private final CompanyDao companyDao;
+    private final CarDao carDao;
+    private final Scanner scanner;
+
+    public CustomerActivity(CompanyDao companyDao, CarDao carDao, Scanner scanner) {
+        this.companyDao = companyDao;
+        this.carDao = carDao;
+        this.scanner = scanner;
+    }
 
     @Override
     public void start() {
