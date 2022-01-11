74a1190dad3fe6aaa4f005b2276eefa4a1289022
Andrii Herman <andrii.herman.w@gmail.com>
2022-01-11 14:32:22 +0200

    Set up flow for start method in MainActivity

--- src/main/java/carsharing/activities/MainActivity.java
+++ src/main/java/carsharing/activities/MainActivity.java
@@ -22,6 +22,41 @@ public class MainActivity implements Activity {
 
     @Override
     public void start() {
-        
+        String option;
+        do {
+            showMenu();
+            option = scanner.nextLine();
+            processOption(option);
+        } while (!"0".equals(option));
+    }
+
+    private void showMenu() {
+        System.out.println("""
+                1. Log in as a manager
+                2. Log in as a customer
+                3. Create a customer
+                0. Exit
+                """);
+    }
+
+    private void processOption(String option) {
+        switch (option) {
+            case "0" -> System.out.println("Bye!");
+            case "1" -> logInAsManager();
+            case "2" -> logInAsCustomer();
+            case "3" -> createCustomerOption();
+        }
+    }
+
+    private void createCustomerOption() {
+
+    }
+
+    private void logInAsCustomer() {
+
+    }
+
+    private void logInAsManager() {
+
     }
 }
