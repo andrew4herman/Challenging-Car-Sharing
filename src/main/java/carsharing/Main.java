3c3f9e5da9eabfc4fc064b2776c3a324f00ab29d
Andrii Herman <andrii.herman.w@gmail.com>
2022-01-11 14:14:13 +0200

    Initialize Main body

--- src/main/java/carsharing/Main.java
+++ src/main/java/carsharing/Main.java
@@ -1,7 +1,30 @@
 package carsharing;
 
+import carsharing.activities.MainActivity;
+import carsharing.database.DBManager;
+import carsharing.database.dao.CarDao;
+import carsharing.database.dao.CompanyDao;
+import carsharing.database.dao.CustomerDao;
+import carsharing.util.CliParser;
+
+import java.util.Optional;
+
 public class Main {
     public static void main(String[] args) {
+        Optional<String> fileName = new CliParser(args).optionOf("-fileName");
 
+        if (fileName.isPresent()) {
+            DBManager dbManager = new DBManager(fileName.get());
+            CompanyDao companyDao = new CompanyDao(dbManager);
+            CarDao carDao = new CarDao(dbManager);
+            CustomerDao customerDao = new CustomerDao(dbManager);
+
+            MainActivity mainActivity = new MainActivity(companyDao, carDao, customerDao);
+            mainActivity.start();
+
+            dbManager.closeConnection();
+        } else {
+            throw new IllegalArgumentException("Incorrect option for -fileName");
+        }
     }
 }
