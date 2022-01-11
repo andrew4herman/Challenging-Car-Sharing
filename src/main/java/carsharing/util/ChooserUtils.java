d192b67c68f472ebed7be7b995a353b51acd5b09
Andrii Herman <andrii.herman.w@gmail.com>
2022-01-11 20:44:07 +0200

    Create ChooserUtils.java

---
+++ src/main/java/carsharing/util/ChooserUtils.java
@@ -0,0 +1,29 @@
+package carsharing.util;
+
+import carsharing.model.Entity;
+import lombok.experimental.UtilityClass;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@UtilityClass
+public class ChooserUtils {
+
+    public <T extends Entity> void outputEntities(List<T> list) {
+        IntStream.iterate(0, i -> i + 1)
+                .limit(list.size())
+                .forEach(i -> System.out.printf("%d. %s%n", i + 1, list.get(i).getName()));
+        System.out.println("0. Back");
+    }
+
+    public <T extends Entity> Optional<T> chooseEntityFrom(List<T> list, int option) {
+        if (option > 0 && option <= list.size()) {
+            return Optional.of(list.get(option - 1));
+        } else if (option == 0) {
+            return Optional.empty();
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+}
