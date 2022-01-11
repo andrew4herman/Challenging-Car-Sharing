48b6d9aba9964238db40fea42f92cb48c7cf6deb
Andrii Herman <andrii.herman.w@gmail.com>
2022-01-11 14:09:59 +0200

    Initialize properties for MainActivity

--- src/main/java/carsharing/activities/MainActivity.java
+++ src/main/java/carsharing/activities/MainActivity.java
@@ -1,9 +1,27 @@
 package carsharing.activities;
 
+import carsharing.database.dao.CarDao;
+import carsharing.database.dao.CompanyDao;
+import carsharing.database.dao.CustomerDao;
+
+import java.util.Scanner;
+
 public class MainActivity implements Activity {
+
+    private final CompanyDao companyDao;
+    private final CarDao carDao;
+    private final CustomerDao customerDao;
+    private final Scanner scanner;
+
+    public MainActivity(CompanyDao companyDao, CarDao carDao, CustomerDao customerDao) {
+        this.companyDao = companyDao;
+        this.carDao = carDao;
+        this.customerDao = customerDao;
+        this.scanner = new Scanner(System.in);
+    }
 
     @Override
     public void start() {
-
+        
     }
 }
