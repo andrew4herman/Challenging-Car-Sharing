b30b0f37ddbcfa76efb052a4231589149d7a47df
Andrii Herman <andrii.herman.w@gmail.com>
2022-01-11 14:48:50 +0200

    Add currentCustomer field to CustomerActivity

--- src/main/java/carsharing/activities/CustomerActivity.java
+++ src/main/java/carsharing/activities/CustomerActivity.java
@@ -2,16 +2,19 @@ package carsharing.activities;
 
 import carsharing.database.dao.CarDao;
 import carsharing.database.dao.CompanyDao;
+import carsharing.model.Customer;
 
 import java.util.Scanner;
 
 public class CustomerActivity implements Activity {
 
+    private final Customer currentCustomer;
     private final CompanyDao companyDao;
     private final CarDao carDao;
     private final Scanner scanner;
 
-    public CustomerActivity(CompanyDao companyDao, CarDao carDao, Scanner scanner) {
+    public CustomerActivity(Customer customer, CompanyDao companyDao, CarDao carDao, Scanner scanner) {
+        this.currentCustomer = customer;
         this.companyDao = companyDao;
         this.carDao = carDao;
         this.scanner = scanner;
